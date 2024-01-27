78fd6271b4d08c8bba01014986ac35aad1dc2561
Tommy Klingerman <davidtklingerman@gmail.com>
2024-01-27 10:22:22 -0500

    Added Constants

---
+++ src/main/java/frc/robot/HelperClasses/Constants.java
@@ -0,0 +1,36 @@
+package frc.robot.HelperClasses;
+
+import edu.wpi.first.wpilibj.util.Color;
+
+public class Constants {
+    public static final String RioCanBus = "rio";
+    public static final String CanivoreCanBus = "Canivore";
+
+    public static final int ShooterMotor1ID = 1;
+    public static final String ShooterMotor1Can = RioCanBus;
+
+    public static class ArmsSubsystemConstants {
+
+    }
+
+    public static class BottomIntakeSubsystemConstants {
+
+    }
+
+    public static class DriveSubsystemConstants {
+
+    }
+
+    public static class LightsSubsystemConstants {
+        public static final Color Orange = new Color(255, 50, 0);
+    }
+
+    public static class ShooterSubsystemConstants {
+
+    }
+
+    public static class TopIntakeSubsystemConstants {
+
+    }
+
+}
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.HelperClasses.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
