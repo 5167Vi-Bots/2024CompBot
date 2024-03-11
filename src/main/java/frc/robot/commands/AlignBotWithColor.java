baf5634814a65087cc8b2ab7c5f5b41d33efe7e1
Tommy Klingerman <davidtklingerman@gmail.com>
2024-03-11 12:59:52 -0400

    Update Alignment commands to use RobotDrive

--- src/main/java/frc/robot/commands/AlignBotWithColor.java
+++ src/main/java/frc/robot/commands/AlignBotWithColor.java
@@ -52,7 +52,7 @@ double xErrorRate = NetworkTableInstance.getDefault().getTable("limelight-front"
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        drive.MoveRobot(0, 0, 0);
+        drive.RobotDrive(0, 0, 0);
     }
   
     // Returns true when the command should end.
